5df710edd40af3ec066f03001d763b9618ceffd8
dimail0880 <dimail4888@gmail.com>
2020-02-20 21:20:47 +0200

    module 10 bugs fixed

--- src/index.js
+++ src/index.js
@@ -1,62 +1,47 @@
 import './styles.css';
-import menu from "./data/menu.json"
+import menu from './data/menu.json';
 import menuTemplate from './templates/menu.hbs';
 
-
-const root = document.querySelector('#root')
+const root = document.querySelector('#root');
 const theme = {
     LIGHT: 'light-theme',
     DARK: 'dark-theme',
 };
 const markup = menuTemplate(menu);
-const body = document.querySelector("body")
-const themeSwitch = document.querySelector('#theme-switch-control')
-themeSwitch.addEventListener("change", switchToTheme)
+const body = document.querySelector('body');
+const themeSwitch = document.querySelector('#theme-switch-control');
+
+
+themeSwitch.addEventListener('change', switchToTheme);
 
 function switchToTheme() {
     if (themeSwitch.checked === true) {
-        body.classList.remove(theme.LIGHT)
-        body.classList.add(theme.DARK)
+        body.classList.remove(theme.LIGHT);
+        body.classList.add(theme.DARK);
+        themeSwitch.checked = true
     } else {
-        body.classList.remove(theme.DARK)
-        body.classList.add(theme.LIGHT)
+        body.classList.remove(theme.DARK);
+        body.classList.add(theme.LIGHT);
+        themeSwitch.checked = false
     }
-
-
+    localStorage.setItem('theme', body.classList.value);
+    localStorage.setItem('theme-check', themeSwitch.checked);
 }
 
-// const settings = {
-//     theme: body.className,
-// };
+function setTheme() {
+    const savedTheme = localStorage.getItem('theme');
+    const savedCheck = localStorage.getItem('theme-check');
+    body.classList.value = savedTheme;
+    themeSwitch.checked = savedCheck
+    if (body.classList.value === 'light-theme') {
+        themeSwitch.checked = false
+    }
+}
 
-// localStorage.setItem('settings', JSON.stringify(settings));
-
-// const savedSettings = localStorage.getItem('settings');
-// const parsedSettings = JSON.parse(savedSettings);
-
-// console.log(parsedSettings);
-
-// Принимает ключ `key` по которому будет произведена выборка.
-// const load = settings => {
-//     try {
-//         const serializedState = localStorage.getItem(settings);
-
-//         return serializedState === null ? undefined : JSON.parse(serializedState);
-//     } catch (err) {
-//         console.error('Get state error: ', err);
-//     }
-// };
-
-// // Принимает ключ `key` и значение `value`.
-// const save = (key, value) => {
-//     try {
-//         const serializedState = JSON.stringify(value);
-//         localStorage.setItem(key, serializedState);
-//     } catch (err) {
-//         console.error('Set state error: ', err);
-//     }
-// };
+console.log(themeSwitch.checked)
 
 
+root.insertAdjacentHTML('afterbegin', markup);
+console.log(themeSwitch.checked)
 
-root.insertAdjacentHTML('afterbegin', markup)
+window.addEventListener("DOMContentLoaded", setTheme)
